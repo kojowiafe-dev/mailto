5b39fb59508da923d01ea4bbd0acb09f2395e4fc
kojowiafe-dev <kojowiafe502@gmail.com>
2025-06-05 13:10:08 +0000

    added google authentication

--- frontend/src/Routes/Register.jsx
+++ frontend/src/Routes/Register.jsx
@@ -5,6 +5,7 @@ import 'react-toastify/ReactToastify.css'
 import { useForm } from 'react-hook-form'
 import AOS from 'aos';
 import api from '../components/api';
+import { GoogleLogin } from '@react-oauth/google';
 
 const Register = () => {
 
@@ -106,20 +107,38 @@ const Register = () => {
         }} action='' className='space-y-8 max-w-xl mx-auto bg-gray-900 p-6 rounded-2xl shadow flex flex-col items-center'>
         <h2 className='text-yellow-300 text-2xl' onSubmit={handleSubmit(onSubmitForm)}>Welcome to eventus!</h2>
         
-        <input {...register('username', { required: true, minLength: 0 })} type="text" placeholder='Username' className='w-full p-3 rounded-md text-yellow-300' value={username} onChange={(e) => setUserName(e.target.value)} />
+        <input {...register('username', { required: true, minLength: 0 })} type="text" placeholder='Username' className='border-b w-full p-3 rounded-md text-yellow-300' value={username} onChange={(e) => setUserName(e.target.value)} />
     
-        <input {...register('email', { required: true })} type="email" placeholder='Email' className='w-full p-3 rounded-md text-yellow-300' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input {...register('email', { required: true })} type="email" placeholder='Email' className='border-b w-full p-3 rounded-md text-yellow-300' value={email} onChange={(e) => setEmail(e.target.value)} />
         
-        <input {...register('password', { required: true, minLength: 8 })} type='password' placeholder='Password' rows='4' className='w-full p-3 rounded-md text-yellow-300' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input {...register('password', { required: true, minLength: 8 })} type='password' placeholder='Password' rows='4' className='border-b w-full p-3 rounded-md text-yellow-300' value={password} onChange={(e) => setPassword(e.target.value)} />
         {
           errors.password && errors.password.type === 'minLength' ? toast.error('Password too short') : null
         }
 
-        <button type='submit' className='bg-gray-50 hover:bg-gray-800 hover:text-yellow-300 text-gray-600 font-bold py-2 px-4 rounded-xl border-gray-600 border-1 my-2 cursor-pointer transition duration-200'>Register</button>
+        <div className='flex flex-col items-center justify-center'>
+          <button type='submit' className='bg-gray-50 hover:bg-gray-800 hover:text-yellow-300 text-gray-600 font-bold py-2 px-4 rounded-xl border-gray-600 border-1 my-2 cursor-pointer transition duration-200'>Register</button>
+          <GoogleLogin
+            theme='filled_blue'
+            text='signup_with'
+            shape='pill'
+            size='large'
+            width='250'
+            height='50'
+            logo_alignment='right'
+            onSuccess={(credentialResponse) => {
+              console.log(credentialResponse);
+            }}
+            onError={() => {
+              console.log('Login Failed');
+            }}
+          />
+        </div>
 
         {/* <a href='#' className='text-gray-200 text-sm underline my-2'>Forgot Password?</a> */}
         <h4 className='text-gray-200'>Already have an account? <Link to="/login" className='underline text-yellow-300'>Login</Link></h4>
     </form>
+    
     <ToastContainer position='top-right' transition={Bounce} autoClose={2000} theme='dark' toastClassName={() => "bg-slate-800 text-white px-6 py-4 rounded-xl shadow-lg animate-slide-in"} bodyClassName={() => "text-sm font-medium"}/>
     </div>
   )
