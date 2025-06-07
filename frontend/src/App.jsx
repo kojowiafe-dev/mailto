b31cb8bc66a6f65f13cfee5054ab457f60212b9c
kojowiafe-dev <kojowiafe502@gmail.com>
2025-06-07 19:33:00 +0000

    styling the footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,14 +13,21 @@ import NotFoundPage from './Routes/NotFoundPage'
 import Footer from './components/Footer'
 import Layout from './Routes/Layout'
 import VerifyEmail from './Routes/VerifyEmail'
+import Hero from './components/Hero'
+import UseWindowResize from './components/use-window-resize'
+import { motion } from 'framer-motion'
 
 
 function CustomRoutes () {
 
   const element = useRoutes([
+    // {
+    //   path: '/home', element: <Hero />
+    // },
     {
       path : '/', element : <LandingPage />,
       children : [
+        // {path: 'home', element: <Hero />},
         {path : 'register', element : <Register />},
         {path : 'login', element : <Login />},
         {path : 'index', element : <Layout />},
@@ -37,8 +44,25 @@ function CustomRoutes () {
 }
 
 const App = () => {
+
+  const windowSize = UseWindowResize();
   return (
-    <div>
+    <div className='min-h-screen flex flex-col bg-gray-300/55 text-gray-950'>
+      <Header />
+      <motion.h1 
+        initial={{ opacity: 0, x: 100 }} 
+        animate={{ opacity: 1, x: 0 }} 
+        transition={{ type: 'spring', stiffness: 100 , damping: 10, delay: 0.7 }} 
+        className='scale-150 font-bold text-gray-400/15 select-none z-0 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none' 
+        style={{ 
+          fontSize: windowSize.width < 800 ? '80px' : '180px',
+          writingMode: windowSize.width < 800 ? 'vertical-rl' : 'horizontal-tb',
+          textOrientation: windowSize.width < 800 ? 'mixed' : 'mixed'
+        }}
+      >
+        EVENTUS
+      </motion.h1>
+      {/* <Hero /> */}
       {/* <Routes>
         <Route path='/home' element={<LandingPage />}>
           <Route path='register' element={<Register />}/>
@@ -48,6 +72,7 @@ const App = () => {
       </Routes> */}
       <CustomRoutes />
 
+      <Footer />
     </div>
   )
 }
