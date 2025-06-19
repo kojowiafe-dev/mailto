6b9d8bda33690318008a41387ef09824c3884175
kojowiafe-dev <kojowiafe502@gmail.com>
2025-06-19 02:53:25 +0000

    deletes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,79 +1,66 @@
-import React from 'react'
-import Header from './components/Header'
-// import Features from './components/Features'
-// import Hero from './components/Hero'
-// import Testimonials from './components/Testimonials'
-// import Pricing from './components/Pricing'
-// import Contact from './components/Contact'
-import Login from './Routes/Login'
-import Register from './Routes/Register'
-import { useRoutes } from 'react-router-dom'
-import LandingPage from './Routes/LandingPage'
-import NotFoundPage from './Routes/NotFoundPage'
-import Footer from './components/Footer'
-import Layout from './Routes/Layout'
-import VerifyEmail from './Routes/VerifyEmail'
-import Hero from './components/Hero'
-import UseWindowResize from './components/use-window-resize'
-import { motion } from 'framer-motion'
-import ForgotPassword from './Routes/ForgotPassword'
+import React from 'react';
+import Header from './components/Header';
+import Login from './Routes/Login';
+import Register from './Routes/Register';
+import { useRoutes } from 'react-router-dom';
+import LandingPage from './Routes/LandingPage';
+import NotFoundPage from './Routes/NotFoundPage';
+import Footer from './components/Footer';
+import VerifyEmail from './Routes/VerifyEmail';
+import UseWindowResize from './components/use-window-resize';
+import { motion } from 'framer-motion';
+import ForgotPassword from './Routes/ForgotPassword';
 
-
-function CustomRoutes () {
-
+function CustomRoutes() {
   const element = useRoutes([
-    // {
-    //   path: '/home', element: <Hero />
-    // },
     {
-      path : '/', element : <LandingPage />,
-      children : [
-        // {path: 'home', element: <Hero />},
-        // {path : 'register', element : <Register />},
-        // {path : 'login', element : <Login />},
-        // {path : 'index', element : <Layout />},
-      ]
+      path: '/',
+      element: <LandingPage />,
     },
     {
-      path: '/register', element: <Register />
+      path: '/register',
+      element: <Register />,
     },
     {
-      path: '/forgot-password', element: <ForgotPassword />
+      path: '/forgot-password',
+      element: <ForgotPassword />,
     },
     {
-      path: '/login', element: <Login />
+      path: '/login',
+      element: <Login />,
     },
     {
-      path: 'verify-email', element: <VerifyEmail />
+      path: 'verify-email',
+      element: <VerifyEmail />,
     },
     {
-      path : '*', element : <NotFoundPage />
+      path: '*',
+      element: <NotFoundPage />,
     },
   ]);
-  return element
+  return element;
 }
 
 const App = () => {
-
   // const { scrollY } = useScroll()
   // const y = useTransform(scrollY, [0, 500], [0, 100])
 
   const windowSize = UseWindowResize();
   return (
-    <div className='min-h-screen flex flex-col background-color text-gray-950 overflow-visible'>
+    <div className="min-h-screen flex flex-col background-color text-gray-950 overflow-visible">
       <Header />
       <motion.h1
         initial={{ opacity: 0, x: 100 }}
         animate={{ opacity: 1, x: 0 }}
         transition={{ type: 'spring', stiffness: 100, damping: 10, delay: 0.7 }}
-        className='fixed z-0 pointer-events-none select-none font-bold text-gray-300/50'
+        className="fixed z-0 pointer-events-none select-none font-bold text-gray-300/50"
         style={{
           top: windowSize.width < 800 ? '20%' : '60%',
           left: '0%',
           transform: 'translate(-50%, -50%)',
           fontSize: windowSize.width < 800 ? '120px' : '180px',
           writingMode: windowSize.width < 800 ? 'vertical-rl' : 'horizontal-tb',
-          textOrientation: 'mixed'
+          textOrientation: 'mixed',
         }}
       >
         EVENTUS
@@ -101,7 +88,7 @@ const App = () => {
 
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
