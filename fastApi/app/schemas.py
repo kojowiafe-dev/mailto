e63364f9c75a7a88cfa3412989847da7aab5acd5
kojowiafe-dev <kojowiafe502@gmail.com>
2025-06-19 19:55:35 +0000

    still making commits

--- fastApi/app/schemas.py
+++ fastApi/app/schemas.py
@@ -65,11 +65,10 @@ class ForgotPasswordRequest(SQLModel):
 
 
 class VerifyResetCodeRequest(SQLModel):
-    email: EmailStr
+    # email: EmailStr
     code: str
 
 
 class ResetPasswordRequest(SQLModel):
-    email: EmailStr
-    code: str
     new_password: str
+    confirm_password: str
