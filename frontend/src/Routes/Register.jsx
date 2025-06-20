6d55ce757517f3064a1b4c0cd1d3c048f71300e6
kojowiafe-dev <kojowiafe502@gmail.com>
2025-06-20 14:39:17 +0000

    fix

--- frontend/src/Routes/Register.jsx
+++ frontend/src/Routes/Register.jsx
@@ -1,142 +1,169 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Bounce, Slide, Zoom, ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/ReactToastify.css'
-import { useForm } from 'react-hook-form'
+import 'react-toastify/ReactToastify.css';
+import { useForm } from 'react-hook-form';
 import AOS from 'aos';
 import api from '../components/api';
 import { GoogleLogin } from '@react-oauth/google';
-import { FaEye, FaEyeSlash, FaHome } from 'react-icons/fa';
+import { FaEye, FaEyeSlash, FaHome, FaUserPlus } from 'react-icons/fa';
 
 const Register = () => {
-
   const navigate = useNavigate();
 
   useEffect(() => {
-    AOS.init({ duration: 1000 })
-  }, [])
+    AOS.init({ duration: 1000 });
+  }, []);
 
-  const { register, handleSubmit, formState: { errors }} = useForm();
+  const {
+    register,
+    formState: { errors },
+  } = useForm();
 
-  const onSubmitForm = (data) => {
-    console.log(data);
-    
-  }
-
-  const [username, setUserName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
-    
 
   return (
-    <div className='p-3' data-aos="fade-up">
-        <form onSubmit={async(e) => {
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 via-white to-blue-200">
+      <form
+        onSubmit={async (e) => {
           e.preventDefault();
           if (!username || !email || !password) {
-            toast.error("Please fill out all the required fields", {
+            toast.error('Please fill out all the required fields', {
               style: {
-                background: "#000",
-                color: "#fff",
-                transition: "all 0.3s ease-in-out"
-              }
+                background: '#000',
+                color: '#fff',
+                transition: 'all 0.3s ease-in-out',
+              },
             });
             return;
           }
           if (password.length > 0 && password.length < 8) {
-            toast.error("Password too short", {
+            toast.error('Password too short', {
               style: {
-                background: "#000",
-                color: "#fff",
-                transition: "all 0.3s ease-in-out"
-              }
-            })
+                background: '#000',
+                color: '#fff',
+                transition: 'all 0.3s ease-in-out',
+              },
+            });
           } else {
             try {
               const response = await api.post('/register', { username, email, password });
-              toast.success("Registration successful", {
-                  style: {
-                    background: "#000",
-                    color: "#fff",
-                    transition: "all 0.3s ease-in-out"
-                  }
-              })
-              navigate('/login')
-              setUserName("");
-              setEmail("");
-              setPassword("");
+              toast.success('Registration successful', {
+                style: {
+                  background: '#000',
+                  color: '#fff',
+                  transition: 'all 0.3s ease-in-out',
+                },
+              });
+              navigate('/login');
+              setUserName('');
+              setEmail('');
+              setPassword('');
               console.log(response.data);
-              
             } catch (error) {
               if (error.response) {
                 const status = error.response.status;
                 const detail = error.response.data.detail;
 
-                if (status === 400 && detail.includes("Username")) {
-                  toast.error("User already exists! Login😊", {
+                if (status === 400 && detail.includes('Username')) {
+                  toast.error('User already exists! Login😊', {
                     style: {
-                      background: "#000",
-                      color: "#fff",
-                      transition: "all 0.3s ease-in-out"
-                    }
-                  })
-                } else if (status === 400 && detail.includes("Email")) {
-                  toast.error("Email already registered!", {
+                      background: '#000',
+                      color: '#fff',
+                      transition: 'all 0.3s ease-in-out',
+                    },
+                  });
+                } else if (status === 400 && detail.includes('Email')) {
+                  toast.error('Email already registered!', {
                     style: {
-                      background: "#000",
-                      color: "#fff",
-                    }
+                      background: '#000',
+                      color: '#fff',
+                    },
                   });
                 } else {
-                  toast.error("Registration failed😥", {
+                  toast.error('Registration failed😥', {
                     style: {
-                      background: "#000",
-                      color: "#fff",
-                    }
-                  })
+                      background: '#000',
+                      color: '#fff',
+                    },
+                  });
                 }
               } else {
-                console.log("Unknown error: ", error);
-                toast.error("Something went wrong", {
+                console.log('Unknown error: ', error);
+                toast.error('Something went wrong', {
                   style: {
-                    background: "#000",
-                    color: "#fff",
-                  }
-                })
+                    background: '#000',
+                    color: '#fff',
+                  },
+                });
               }
             }
           }
-        }} action='' className='space-y-8 max-w-xl mx-auto nav-color border-1 border-gray-600 p-6 rounded-2xl shadow flex flex-col items-center'>
-        <h2 className='button-color text-2xl' onSubmit={handleSubmit(onSubmitForm)}>Welcome to eventus!</h2>
-        
-        <input {...register('username', { required: true, minLength: 0 })} type="text" placeholder='Username' className='border-b w-full p-3 rounded-md button-color' value={username} onChange={(e) => setUserName(e.target.value)} />
-    
-        <input {...register('email', { required: true })} type="email" placeholder='Email' className='border-b w-full p-3 rounded-md button-color' value={email} onChange={(e) => setEmail(e.target.value)} />
-        
-        <div className='relative w-full'>
-          <input {...register('password', { required: true, minLength: 8 })} type={showPassword ? 'text' : 'password'} placeholder='Password' rows='4' className='border-b w-full p-3 rounded-md button-color' value={password} onChange={(e) => setPassword(e.target.value)} />
+        }}
+        action=""
+        className="w-full max-w-md mx-auto bg-white rounded-3xl shadow-2xl p-10 flex flex-col items-center space-y-7 border border-blue-100"
+      >
+        <div className="flex flex-col items-center mb-2">
+          <div className="bg-blue-100 p-4 rounded-full mb-2 shadow">
+            <FaUserPlus className="text-blue-500 text-3xl" />
+          </div>
+          <h2 className="text-3xl font-extrabold text-blue-700 mb-1">Create Account</h2>
+          <p className="text-gray-500 text-base">Sign up to get started with Eventus</p>
+        </div>
+        <input
+          {...register('username', { required: true, minLength: 0 })}
+          type="text"
+          placeholder="Username"
+          className="border border-blue-200 w-full p-3 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none transition bg-blue-50 text-gray-900 placeholder-gray-400"
+          value={username}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <input
+          {...register('email', { required: true })}
+          type="email"
+          placeholder="Email"
+          className="border border-blue-200 w-full p-3 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none transition bg-blue-50 text-gray-900 placeholder-gray-400"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <div className="relative w-full">
+          <input
+            {...register('password', { required: true, minLength: 8 })}
+            type={showPassword ? 'text' : 'password'}
+            placeholder="Password"
+            className="border border-blue-200 w-full p-3 rounded-lg focus:ring-2 focus:ring-blue-400 focus:outline-none transition bg-blue-50 text-gray-900 placeholder-gray-400"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button
             type="button"
             onClick={() => setShowPassword(!showPassword)}
-            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-blue-400 hover:text-blue-600"
           >
-            {showPassword ? <FaEyeSlash/> : <FaEye />}
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
           </button>
         </div>
-        {
-          errors.password && errors.password.type === 'minLength' ? toast.error('Password too short') : null
-        }
-
-        <div className='flex flex-col items-center justify-center'>
-          <button type='submit' className='bg-button-color text-color font-bold py-2 px-4 rounded-xl my-2 cursor-pointer transition duration-200'>Register</button>
+        {errors.password && errors.password.type === 'minLength'
+          ? toast.error('Password too short')
+          : null}
+        <button
+          type="submit"
+          className="w-full bg-gradient-to-r from-blue-500 to-blue-700 text-white font-bold py-3 px-4 rounded-xl my-2 cursor-pointer transition duration-200 shadow-md hover:from-blue-600 hover:to-blue-800 hover:scale-105 active:scale-95"
+        >
+          Register
+        </button>
+        <div className="w-full flex flex-col items-center gap-2">
           <GoogleLogin
-            theme='filled_blue'
-            text='signup_with'
-            shape='pill'
-            size='large'
-            width='250'
-            height='50'
-            logo_alignment='right'
+            theme="filled_blue"
+            text="signup_with"
+            shape="pill"
+            size="large"
+            width="250"
+            height="50"
+            logo_alignment="right"
             onSuccess={(credentialResponse) => {
               console.log(credentialResponse);
             }}
@@ -144,19 +171,33 @@ const Register = () => {
               console.log('Login Failed');
             }}
           />
+          <h4 className="text-gray-500 text-sm">
+            Already have an account?{' '}
+            <Link to="/login" className="underline text-blue-600 font-semibold">
+              Login
+            </Link>
+          </h4>
+          <Link
+            to="/"
+            className="text-blue-500 flex gap-2 items-center justify-center text-sm font-medium hover:underline"
+          >
+            Explore Eventus
+            <FaHome />
+          </Link>
         </div>
-
-        {/* <a href='#' className='text-gray-200 text-sm underline my-2'>Forgot Password?</a> */}
-        <h4 className='text-gray-200'>Already have an account? <Link to="/login" className='underline button-color'>Login</Link></h4>
-        <Link to="/" className='button-color flex gap-2 items-center justify-center'>
-          Explore Eventus
-          <FaHome />
-        </Link>
-    </form>
-    
-    <ToastContainer position='top-right' transition={Bounce} autoClose={2000} theme='dark' toastClassName={() => "bg-slate-800 text-white px-6 py-4 rounded-xl shadow-lg animate-slide-in"} bodyClassName={() => "text-sm font-medium"}/>
+        <ToastContainer
+          position="top-right"
+          transition={Bounce}
+          autoClose={2000}
+          theme="light"
+          toastClassName={() =>
+            'bg-slate-800 text-white px-6 py-4 rounded-xl shadow-lg animate-slide-in'
+          }
+          bodyClassName={() => 'text-sm font-medium'}
+        />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
