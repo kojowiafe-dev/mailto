f34939bf7e938fc24c3201e3b6ca31cb2da84c5a
kojowiafe-dev <kojowiafe502@gmail.com>
2025-06-23 14:11:36 +0000

    back to business

--- fastApi/app/main.py
+++ fastApi/app/main.py
@@ -10,10 +10,10 @@ app = FastAPI()
 
 
 origins = [
-    "http://10.11.24.187:5173",
-    "http://10.11.24.187:5173/register",
-    "http://10.11.24.187:5173/login",
-    "http://10.11.24.187:5173/index",
+    "http://10.11.24.223:5173",
+    "http://10.11.24.223:5173/register",
+    "http://10.11.24.223:5173/login",
+    "http://10.11.24.223:5173/index",
     "http://localhost:5173",
     "http://localhost:5173/register",
     "http://localhost:5173/login",
