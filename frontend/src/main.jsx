1bd1973220f952604e19b0c65d56ba0dbff5ace3
kojowiafe-dev <kojowiafe502@gmail.com>
2025-06-24 17:54:20 +0000

    fixing forgot password

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,19 @@
 // import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { GoogleOAuthProvider } from '@react-oauth/google'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import ResetProvider from './context/ResetPasswordContext.jsx';
 
-const CLIENT_ID = "90556896493-0h3c63u12aopg87ugr3jrbbtvv89m875.apps.googleusercontent.com"
-
+const CLIENT_ID = '90556896493-0h3c63u12aopg87ugr3jrbbtvv89m875.apps.googleusercontent.com';
 
 createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId={CLIENT_ID}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ResetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ResetProvider>
   </GoogleOAuthProvider>
-)
+);
