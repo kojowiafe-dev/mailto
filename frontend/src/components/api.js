51e8dd31b716762ae3c4a95096e81dcfb44272d4
kojowiafe-dev <kojowiafe502@gmail.com>
2025-06-25 16:39:44 +0000

    commit

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://localhost:8000',
-  baseURL: 'http://10.11.24.7:8000',
+  baseURL: 'http://localhost:8000',
+  // baseURL: 'http://10.11.24.7:8000',
 });
 
 export default api;
