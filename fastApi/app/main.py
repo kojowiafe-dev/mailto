dbd857dc59eb071123dae46c410bd8ec3459e8ca
kojowiafe-dev <kojowiafe502@gmail.com>
2025-06-26 08:37:21 +0000

    work

--- fastApi/app/main.py
+++ fastApi/app/main.py
@@ -10,10 +10,6 @@ app = FastAPI()
 
 
 origins = [
-    "http://10.11.24.7:8000",
-    "http://10.11.24.7:8000/register",
-    "http://10.11.24.7:8000/login",
-    "http://10.11.24.7:8000/index",
     "http://localhost:5173",
     "http://localhost:5173/register",
     "http://localhost:5173/login",
