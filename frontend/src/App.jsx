0edc17c3dd7843d6b0930cb3c656f9e0a0efe5d9
kojowiafe-dev <kojowiafe502@gmail.com>
2025-07-02 22:13:24 +0000

    push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import LandingPage from './Routes/LandingPage';
 import Features from './Routes/Features';
 import Industries from './Routes/Industries';
 import Demo from './Routes/Demo';
+import { Spotlight } from './components/ui/spotlight';
+// import { SparklesCore } from './components/ui/spotlight';
 import { Toaster } from 'sonner';
 import Footer from './Routes/Footer';
 import Contact from './Routes/Contact';
@@ -43,6 +45,7 @@ const App = () => {
   return (
     <div className="min-h-screen flex flex-col background-color text-gray-950 overflow-visible">
       <Toaster position="top-center" richColors />
+      <Spotlight className="absolute inset-0 z-0" />
       <Header />
 
       <CustomRoutes />
