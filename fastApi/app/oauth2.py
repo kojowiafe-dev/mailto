d6c06727a8bfffd09df6a18735a1550244a1cf7e
kojowiafe-dev <kojowiafe502@gmail.com>
2025-07-14 15:00:49 +0000

    work

--- fastApi/app/oauth2.py
+++ fastApi/app/oauth2.py
@@ -28,17 +28,43 @@ async def protected_route(token: str = Depends(oauth2_scheme)):
 async def read_users_me(token: Annotated[str, Depends(oauth2_scheme)]):
     return {"token": token}
     
+# def get_current_user(session: database.SessionLocal, token: str = Depends(oauth2_scheme)):
+#     try:
+#         payload = jwt.decode(token, token_access.SECRET_KEY, algorithms=[token_access.ALGORITHM])
+#         email: str = payload.get("sub")
+#         if email is None:
+#             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+
+#         user = session.exec(select(models.User).where(models.User.email == email)).first()
+#         if user is None:
+#             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+#         return user
+
+#     except JWTError:
+#         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Token is invalid")
+    
+    
+
 def get_current_user(session: database.SessionLocal, token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
     try:
         payload = jwt.decode(token, token_access.SECRET_KEY, algorithms=[token_access.ALGORITHM])
-        email: str = payload.get("sub")
-        if email is None:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+        user_id: int = payload.get("user_id")
 
-        user = session.exec(select(models.User).where(models.User.email == email)).first()
-        if user is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-        return user
+        if user_id is None:
+            raise credentials_exception
 
     except JWTError:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Token is invalid")
+        raise credentials_exception
+
+    user = session.exec(select(models.User).where(models.User.id == user_id)).first()
+
+    if user is None:
+        raise credentials_exception
+
+    return user
