9bddae9da2a1b2f91e36f9f7f5b277dc5a275890
kojowiafe-dev <kojowiafe502@gmail.com>
2025-07-14 14:50:50 +0000

    still fixing

--- fastApi/app/oauth2.py
+++ fastApi/app/oauth2.py
@@ -17,7 +17,7 @@ router = APIRouter(
 @router.post("/token")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     if form_data.username != "user" or form_data.password != "pass":
-        raise HTTPException(status_code=400, detail="Invalid credentials")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid credentials")
     return {"access_token": "mytoken", "token_type": "bearer"}
 
 @router.get("/protected")
@@ -33,12 +33,12 @@ def get_current_user(session: database.SessionLocal, token: str = Depends(oauth2
         payload = jwt.decode(token, token_access.SECRET_KEY, algorithms=[token_access.ALGORITHM])
         email: str = payload.get("sub")
         if email is None:
-            raise HTTPException(status_code=401, detail="Invalid token")
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
 
         user = session.exec(select(models.User).where(models.User.email == email)).first()
         if user is None:
-            raise HTTPException(status_code=404, detail="User not found")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
         return user
 
     except JWTError:
-        raise HTTPException(status_code=403, detail="Token is invalid")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Token is invalid")
