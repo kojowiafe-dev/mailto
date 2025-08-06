73807980713147f88ab6860870e94da80c454546
kojowiafe-dev <kojowiafe502@gmail.com>
2025-08-06 19:35:44 +0000

    working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,24 @@
-import React, { useState } from 'react';
-import { Routes, Route, useRoutes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import LandingPage from './Routes/LandingPage';
 import Solutions from './Routes/Solutions';
 import Industries from './Routes/Industries';
 import Demo from './Routes/Demo';
 import GetStarted from './Routes/GetStarted';
-import AIMailCompose from './Routes/AIMailCompose';
-import { Spotlight } from './components/ui/spotlight';
-// import { SparklesCore } from './components/ui/spotlight';
-// import SaveProgressPrompt from './Routes/SaveProgressPrompt';
-import { Toaster } from 'sonner';
-import Footer from './Routes/Footer';
-import Contact from './Routes/Contact';
-import Header from './Routes/Header';
-import Login from './Routes/Login';
-import Register from './Routes/Register';
-import Profile from './Routes/Profile';
+import AIMailCompose from './Pages/AIMailCompose';
+import Contact from './Pages/Contact';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
 import ForgotPassword from './Routes/ForgotPassword';
-import ResetPassword from './Routes/ResetPassword';
+import ResetPassword from './Pages/ResetPassword';
 import VerifyResetCode from './Routes/VerifyResetCode';
 import ProtectedRoute from './Routes/ProtectedRoute';
 import Hero from './Routes/Hero';
 import GoogleSuccess from './Routes/GoogleSuccess';
-// import GmailSuccess from './Routes/GmailSuccess';
-// import GmailStatusBadge from './Routes/GmailStatusBadge';
-
-// function CustomRoutes() {
-//   const element = useRoutes([
-//     {
-//       path: '/',
-//       element: <LandingPage />,
-//     }
-//   ]);
-//   return element;
-// }
 
 const App = () => {
   return (
     <Routes>
       {/* Public Routes */}
-      {/* <Route path="/*" element={<CustomRoutes />} /> */}
       <Route element={<LandingPage />}>
         <Route path="/" element={<Hero />} />
         <Route path="/login" element={<Login />} />
@@ -53,8 +32,6 @@ const App = () => {
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/verify-reset-code" element={<VerifyResetCode />} />
         <Route path="/google-linked-success" element={<GoogleSuccess />} />
-        {/* <Route path="/gmail/success" element={<GmailSuccess />} /> */}
-        {/* <Route path="/gmail/success" element={<GmailStatusBadge />} /> */}
         <Route
           path="/ai-mail-compose"
           element={
@@ -64,7 +41,6 @@ const App = () => {
           }
         />
       </Route>
-      {/* <Route path="/" element={<LandingPage />} /> */}
     </Routes>
   );
 };
