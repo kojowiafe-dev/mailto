92b4b5d4365997f7e97405d98dd7be3ee2770012
kojowiafe-dev <kojowiafe502@gmail.com>
2025-08-08 18:14:22 +0000

    pushing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={CLIENT_ID}>
     <AuthProvider>
       <ResetProvider>
-        <BrowserRouter basename="/mailto">
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </ResetProvider>
