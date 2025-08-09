3b9b17a55703c734546f3ca1d186d135324747fd
kojowiafe-dev <kojowiafe502@gmail.com>
2025-08-09 21:18:49 +0000

    third push

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base: "/mailto",
+  base: process.env.VITE_BASE_PATH || "/mailto",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
